5520e8c6913d002a0b832d79db62f1bfa2f2fe00
Andre Duarte <andreduarte@theagilemonkeys.com>
2024-07-12 17:51:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.get('/health', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App has started in port ${port}`);
 });
