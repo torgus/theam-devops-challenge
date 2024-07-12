6bf035126dc521725a35f32e946822ac8105f3f8
Andre Duarte <andreduarte2010@gmail.com>
2024-07-12 17:49:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/health', (req, res) => {
+  res.status(200).send('ok');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
